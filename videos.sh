d6d2f81e82b73c3e46ab4b025dc59fbd6543629a
jaseelan <93515798+jaseelan@users.noreply.github.com>
2023-06-18 20:21:32 +0530

    Create videos.sh

---
+++ videos.sh
@@ -0,0 +1,6 @@
+bash script for grapping videos locations
+
+#!/bin/bash
+#This script is intended to find virtually all video file formats.
+find /. -type f | grep -E "\.webm$|\.flv$|\.vob$|\.ogg$|\.ogv$|\.drc$
+|\.gifv$|\.mng$|\.avi$|\.mov$|\.qt$|\.wmv$|\.yuv$|\.rm$|\.rmvb$|/.asf$|\.amv$|\.mp4$|\.m4v$|\.mp*$|\.m?v$|\.svi$|\.3gp$|\.flv$|\.f4v$"
